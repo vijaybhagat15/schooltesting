8641c8777946543b5f85ea4d0386e567488d1a24
vijaybhagat15 <vijaybhagatmitlab@gmail.com>
2025-05-12 01:23:45 +0530

    finel  cummit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 import Section1 from "../components/Sections_for_Home/Section1";
 import Section2 from "../components/Sections_for_Home/Section2";
-import Section3 from "../components/Sections_for_Home/Section3";
+// import Section3 from "../components/Sections_for_Home/Section3";
 import Section4 from "../components/Sections_for_Home/Section4";
-import Section5 from "../components/Sections_for_Home/Section5";
-import Section6 from "../components/Sections_for_Home/Section6";
+// import Section5 from "../components/Sections_for_Home/Section5";
+// import Section6 from "../components/Sections_for_Home/Section6";
 const Home: React.FC = () => {
   return (
     <div>
